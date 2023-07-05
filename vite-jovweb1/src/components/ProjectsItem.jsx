ac190ad84979a8a2287212e8bb739a28ecbe269e
Jov Bautista <jovellmariebautista@protonmail.com>
2023-07-05 06:27:18 -0700

    projects component

--- vite-jovweb1/src/components/ProjectsItem.jsx
+++ vite-jovweb1/src/components/ProjectsItem.jsx
@@ -4,7 +4,11 @@ const ProjectsItem = ({ img, title }) => {
   return (
     <div>
       <img src={img} alt={title} />
-      <h2>{title}</h2>
+      <div className='relative flex item-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#001b5e]'>
+        <h3>
+          {title}
+        </h3>
+      </div>
     </div>
   );
 };
