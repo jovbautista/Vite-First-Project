25d29db6ef21bf9653e543b86f3713acd69654f5
Jov Bautista <jovellmariebautista@protonmail.com>
2023-07-16 05:50:38 -0700

    update

--- vite-jovweb1/src/components/SideNav.jsx
+++ vite-jovweb1/src/components/SideNav.jsx
@@ -30,7 +30,7 @@ const SideNav = () => {
             <span className='pl-4'>Home</span>
           </a>
           <a
-            href="interest"
+            href="#interest"
             className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'
             onClick={handleMenuItemClick}
           >
